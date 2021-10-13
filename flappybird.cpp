c57374f1a56cd2f28c563ab14dbcf23febf24ef7
DESKTOP-MF5FVPM\Lenovo <kariyaw-im19081@stu.kln.ac.lk>
2021-10-13 11:46:16 +0530

    added project description

--- flappybird.cpp
+++ flappybird.cpp
@@ -1,3 +1,35 @@
+/*
+Presentation Date: 26/10/2021
+Presentation Duration: 10 Minutes
+
+
+It is expected you to demonstrate everything you have learned during this course in your final
+project. It should be a fully-fledged C++ program, written entirely from the scratch by you,
+satisfying the requirements specified below. Groups come up with any idea that they want for
+the final project. Possible ideas include:
+
+
+
+	Games (can be multi-player or single-player):
+	Card games (e.g. Blackjack, Poker)
+	Reversi
+	Gomoku
+	Connect Four
+	Any other game
+
+
+
+Your project should be large enough to take about 40-50 hours of coding.
+It is very important that you write easily readable, well-designed code.
+Submit a report, with some basic documentation and instructions on how to play your game.
+Also include in this report what problems you had with your project, what the challenges
+were, and what would you have done differently if you could do it again. Include your well
+design code as an appendix to the report.
+
+
+
+
+*/
 #include<iostream>
 #include<conio.h>
 #include<dos.h>
